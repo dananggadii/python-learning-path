cd8403d1c90f0b420a2984879ef6c9b693ffde6b
Danangadi26 <zeroonex72017@gmail.com>
2024-11-01 21:32:08 +0700

    variable and type data

--- main.py
+++ main.py
@@ -1,2 +1,11 @@
-# Print the data type of revenue_on_target
-print(type(revenue_on_target))
+# Create a string variable called name
+name = 'John Smith'
+
+# Create an integer variable called age
+age = 47
+
+# Create a string variable called address
+address = 'The Shard, London, SE1 9SG'
+
+# Print their information
+print(name, age,  address)
