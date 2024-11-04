3ed4b2439d180e2345e283656332c0d8a3e5eab0
Danangadi26 <zeroonex72017@gmail.com>
2024-11-04 13:53:36 +0700

    update list element

--- main.py
+++ main.py
@@ -47,3 +47,8 @@ print(most_popular_course)
 
 # =================================================================================================
 
+# Create the playlist variable
+playlist = [1, 'Blinding Lights', 2, 'One Dance', 3, 'Uptown Funk']
+
+# Print the list
+print(playlist)
