657c4ec894bbea77fd50f7488242be379889f950
Danangadi26 <zeroonex72017@gmail.com>
2024-11-04 16:26:44 +0700

    update tuple

--- main.py
+++ main.py
@@ -85,3 +85,11 @@ playlist['Usher'] = 'Yeah!'
 
 # Print the songs in the playlist
 print(playlist.values())
+
+# =================================================================================================
+
+# Create a tuple
+q3_financials = (325780, 1041, 4271599)
+
+# Print the tuple
+print(q3_financials)
