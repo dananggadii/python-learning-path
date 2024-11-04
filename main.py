63f214e564ed1ab318715d1403295d7e857d9a8a
Danangadi26 <zeroonex72017@gmail.com>
2024-11-04 13:07:34 +0700

    update replace and lower function

--- main.py
+++ main.py
@@ -29,3 +29,21 @@ Keep up the great work.
 # Print multiple string
 print(review_one)
 print(review_two)
+
+# =================================================================================================
+
+most_popular_course = "Intro to Embeddings with the OpenAI API"
+
+# Update the first word
+most_popular_course = most_popular_course.replace("Intro", "Introduction")
+
+# Swap spaces for underscores
+most_popular_course = most_popular_course.replace(" ", "_")
+
+# Change to lowercase
+most_popular_course = most_popular_course.lower()
+
+print(most_popular_course)
+
+# =================================================================================================
+
