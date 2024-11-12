5190122a2d193571d070178ddc4e4791b0193874
Danangadi26 <zeroonex72017@gmail.com>
2024-11-12 17:54:54 +0700

    update for loop with condition

--- fundamental.py
+++ fundamental.py
@@ -148,3 +148,23 @@ for tickets in range(1, max_capacity + 1):
     # Add one to tickets_sold in each iteration
     tickets_sold += 1
 print("Sold out:", tickets_sold, "tickets sold!")
+
+# =================================================================================================
+
+# Loop through the dictionary's keys and values
+for key, value in courses.items():
+    
+    # Check if the value is "AI"
+    if value == "AI":
+        print(key, "is an AI course")
+    
+    # Check if the value is "Programming"
+    elif value == "Programming":
+        print(key, "is a Programming course")
+    
+    # Otherwise, print that it is a "Data Engineering" course
+    else:
+        print(key, "is a Data Engineering course")
+
+# =================================================================================================
+
