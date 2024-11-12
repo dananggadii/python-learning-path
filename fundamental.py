70c07181c27793dbabec4cf20c720846c2f5d2a4
Danangadi26 <zeroonex72017@gmail.com>
2024-11-12 17:49:50 +0700

    update

--- fundamental.py
+++ fundamental.py
@@ -134,3 +134,17 @@ user_ids = ["T42YG4KTK", "VTQ39IDQ0", "CRL11YUWX",
 for user_id in user_ids:
     # Print the user_id
     print(user_id)
+
+#  =================================================================================================
+
+# Create the tickets_sold variable
+tickets_sold = 0
+
+# Create the max_capacity variable
+max_capacity = 30
+
+# Loop through a range up to and including max_capacity's value
+for tickets in range(1, max_capacity + 1):
+    # Add one to tickets_sold in each iteration
+    tickets_sold += 1
+print("Sold out:", tickets_sold, "tickets sold!")
