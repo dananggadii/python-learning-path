608ec1f78408cd1edecac4601dd2d4303b01985c
Danangadi26 <zeroonex72017@gmail.com>
2024-11-12 17:45:34 +0700

    update for loop

--- fundamental.py
+++ fundamental.py
@@ -125,6 +125,12 @@ elif rent > max_rent:
 else:
     print("This looks promising!")
 
-
 # =================================================================================================
 
+user_ids = ["T42YG4KTK", "VTQ39IDQ0", "CRL11YUWX", 
+            "K6Y5URXLR", "V4XCBER7V", "IOGQWC61K"]
+
+# Loop through user_ids
+for user_id in user_ids:
+    # Print the user_id
+    print(user_id)
