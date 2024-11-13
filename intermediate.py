244620e7af3d208c6cd6060df1424b685cfc6e2b
Danangadi26 <zeroonex72017@gmail.com>
2024-11-13 16:24:24 +0700

    update

--- intermediate.py
+++ intermediate.py
@@ -156,3 +156,34 @@ print(help(convert_data_structure))
 
 # =================================================================================================
 
+# Define a function called concat
+def concat(*args):
+  
+  # Create an empty string
+  result = ""
+  
+  # Iterate over the Python args tuple
+  for arg in args:
+    result += " " + arg
+  return result
+
+# Call the function
+print(concat("Python", "is", "great!"))
+
+# =================================================================================================
+
+# Define a function called concat
+def concat(**kwargs):
+  
+  # Create an empty string
+  result = ""
+  
+  # Iterate over the Python kwargs
+  for kwarg in kwargs.values():
+    result += " " + kwarg
+  return result
+
+# Call the function
+print(concat(start="Python", middle="is", end="great!"))
+
+# =================================================================================================
