516effc8a484f072eb7dcb2b4478e8f4ee8bba11
Danangadi26 <zeroonex72017@gmail.com>
2024-11-13 14:49:33 +0700

    update

--- intermediate.py
+++ intermediate.py
@@ -9,10 +9,6 @@ print(num_courses)
 
 course_completions = [97, 83, 121, 205, 56, 174, 92, 117, 164]
 
-# Find the number of courses
-num_courses = len(course_completions)
-print(num_courses)
-
 # =================================================================================================
 
 
