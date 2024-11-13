b1462af3523fac465c2a3ed1aa1541b56a99514b
Danangadi26 <zeroonex72017@gmail.com>
2024-11-13 15:58:39 +0700

    update

--- intermediate.py
+++ intermediate.py
@@ -74,3 +74,38 @@ list_value = [20, 4, 11, 9, 10]
 max_values = find_max(list_value)
 
 print(max_values)
+
+# =================================================================================================
+
+# Create the clean_string function
+def clean_string(text):
+  
+  # Replace spaces with underscores
+  no_spaces = text.replace(" ", "_")
+  
+  # Convert to lowercase
+  clean_text = no_spaces.lower()
+  
+  # Return the final text as an output
+  return clean_text
+
+converted_text = clean_string("I LoVe dATaCamP!")
+print(converted_text)
+
+# =================================================================================================
+
+password = "not_very_secure_2023"
+
+# Define the password_checker function
+def password_checker(submission):
+  
+  # Check that the password variable and the submission match
+  if password == submission:
+    print("Successful login!")
+  
+  # Otherwise, print "Incorrect password"
+  else:
+    print("Incorrect password")
+
+# Call the function    
+password_checker("NOT_VERY_SECURE_2023")
