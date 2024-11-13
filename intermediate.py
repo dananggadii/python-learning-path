6b78c9ee6d82ebe6e375b8b65dc735ba5596aab7
Danangadi26 <zeroonex72017@gmail.com>
2024-11-13 15:17:23 +0700

    update

--- intermediate.py
+++ intermediate.py
@@ -28,3 +28,11 @@ print(deadline)
 
 # =================================================================================================
 
+import pandas as pd
+
+sales = {
+  "user_id" : ["KM37", "PR19", "YU88"],
+  "order_value" : [197.75, 208.21, 134.99]
+}
+
+sales_df = pd.DataFrame(sales)
