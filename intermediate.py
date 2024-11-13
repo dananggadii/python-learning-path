a62c0a51e037f8039fe199c211add539f2e87501
Danangadi26 <zeroonex72017@gmail.com>
2024-11-13 16:14:17 +0700

    update

--- intermediate.py
+++ intermediate.py
@@ -109,3 +109,25 @@ def password_checker(submission):
 
 # Call the function    
 password_checker("NOT_VERY_SECURE_2023")
+
+# =================================================================================================
+
+# Create the convert_data_structure function
+def convert_data_structure(data, data_type="list"):
+  
+  # If data_type is "tuple"
+  if data_type == "tuple":
+    data = tuple(data)
+  
+  # Else if data_type is set, convert to a set
+  elif data_type == "set":
+    data = set(data)
+  else:
+    data = list(data)
+  return data
+
+# Call the function to convert to a set
+convert_data_structure({"a", 1, "b", 2, "c", 3}, data_type="set")
+
+# =================================================================================================
+
