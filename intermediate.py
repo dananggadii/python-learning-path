eca18f867e0e93ea39e4619f3f0595240050a8b4
Danangadi26 <zeroonex72017@gmail.com>
2024-11-15 22:52:45 +0700

    update lambda function

--- intermediate.py
+++ intermediate.py
@@ -187,3 +187,11 @@ def concat(**kwargs):
 print(concat(start="Python", middle="is", end="great!"))
 
 # =================================================================================================
+
+sale_price = 29.99
+
+# Define a lambda function called add_tax
+add_tax = lambda x: x * 1.2
+
+# Call the lambda function
+print(add_tax(sale_price))
