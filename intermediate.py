56e73f6ea1f392c8d9dbc32f81417a03179daa76
Danangadi26 <zeroonex72017@gmail.com>
2024-11-15 23:13:57 +0700

    error handling

--- intermediate.py
+++ intermediate.py
@@ -205,3 +205,39 @@ print((lambda x: x * 1.2)(sale_price))
 
 # =================================================================================================
 
+sales_prices = [29.99, 9.95, 14.50, 39.75, 60.00]
+
+# Create add_taxes to add 20% to each item in sales_prices
+add_taxes = map(lambda x: x * 1.2, sales_prices)
+
+# Use add_taxes to return a new list with updated values
+print(list(add_taxes))
+
+# =================================================================================================
+
+def snake_case(text):
+  # Attempt to clean the text
+  try:
+    # Swap spaces for underscores
+    clean_text = text.replace(" ", "_")
+    clean_text = clean_text.lower()
+  # Run this code if an error occurs
+  except:
+    print("The snake_case() function expects a string as an argument, please check the data type provided.")
+    
+snake_case("User Name 187")
+
+# =================================================================================================
+
+def snake_case(text):
+  # Check the data type
+  if type(text) == str:
+    clean_text = text.replace(" ", "_")
+    clean_text = clean_text.lower()
+  else:
+    # Return a TypeError error if the wrong data type was used
+    raise TypeError("The snake_case() function expects a string as an argument, please check the data type provided.")
+    
+snake_case("User Name 187")
+
+
